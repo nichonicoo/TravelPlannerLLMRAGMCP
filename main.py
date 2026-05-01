37418189a985f89dee5941a6649ca1e550ee9973
Winsen Dharma <163168188+wedew14@users.noreply.github.com>
2026-05-01 09:22:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,39 +13,6 @@ while True:
     
     if query == 'exit':
         break
-    # if retriever is None:
-    #     # heuristik murah: keyword RAG
-    #     if any(k in query.lower() for k in ["prospektus", "saham", "laporan", "risiko"]):
-    #         print("🧠 Loading RAG engine...")
-    #         from RAG.rag_setup import setup_rag
-    #         retriever = setup_rag()
-    #         print("✅ RAG loaded")
             
     answer = langchain_router(query, retriever)
     print("Bot:", answer)
-
-# INIT RAG
-# retriever = setup_rag()
-
-# print("\n🔍 DEBUG: Testing retriever untuk query 'kantor pusat'")
-# retrieved_docs = retriever.invoke("kantor pusat")
-
-# for d in retrieved_docs:
-#     print("\n----- CHUNK -----")
-#     print("HALAMAN:", d.metadata.get("page"))
-#     print(d.page_content[:400], "...")
-#     print("-----------------\n")
-
-# print("🔥 DEBUG DONE — lanjut chatbot…\n")
-
-# print("🔥 AI Stock Assistant ready!")
-# print("Ketik 'exit' untuk keluar.\n")
-
-# while True:
-#     query = input("User: ")
-    
-#     if query == "exit":
-#         break
-
-#     answer = langchain_router(query, retriever, model)
-#     print("Bot:", answer)
